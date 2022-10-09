fc1655b6197df3922b13a374eaddf8173509590d
Niklas Kuder <nkqdr@users.noreply.github.com>
2022-10-09 21:41:37 +0200

    Initial Commit

---
+++ Monity/MonityApp.swift
@@ -0,0 +1,20 @@
+//
+//  MonityApp.swift
+//  Monity
+//
+//  Created by Niklas Kuder on 09.10.22.
+//
+
+import SwiftUI
+
+@main
+struct MonityApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
