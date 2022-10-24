fd22f12c7eb563860f7dd79110e9439b422e8272
Niklas Kuder <nkqdr@users.noreply.github.com>
2022-10-24 19:58:36 +0200

    Added text if Savingscategories are empty

--- Monity/View/SavingsView.swift
+++ Monity/View/SavingsView.swift
@@ -10,25 +10,40 @@ import SwiftUI
 struct SavingsView: View {
     @ObservedObject private var content = SavingsViewModel()
     
+    @ViewBuilder
+    var scrollViewContent: some View {
+        if content.categories.isEmpty {
+            VStack {
+                Text("No Savings categories defined.")
+                Text("Go to Settings > Savings to define your categories.")
+            }
+            .foregroundColor(.secondary)
+            .padding()
+            .multilineTextAlignment(.center)
+        } else {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(), GridItem()]) {
+                    ForEach(content.categories) { category in
+                        GroupBox(label: Text(category.wrappedName).groupBoxLabelTextStyle()) {
+                            Circle()
+                                .frame(width: 20)
+                                .foregroundColor(.red)
+                        }
+                        .groupBoxStyle(CustomGroupBox())
+                        .frame(minHeight: 200)
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+    
     var body: some View {
         NavigationView {
             ZStack {
                 Color(UIColor.systemGroupedBackground)
                     .edgesIgnoringSafeArea(.all)
-                ScrollView {
-                    LazyVGrid(columns: [GridItem(), GridItem()]) {
-                        ForEach(content.categories) { category in
-                            GroupBox(label: Text(category.wrappedName).groupBoxLabelTextStyle()) {
-                                Circle()
-                                    .frame(width: 20)
-                                    .foregroundColor(.red)
-                            }
-                            .groupBoxStyle(CustomGroupBox())
-                            .frame(minHeight: 200)
-                        }
-                    }
-                    .padding()
-                }
+                scrollViewContent
             }
             .navigationTitle("Savings")
         }
