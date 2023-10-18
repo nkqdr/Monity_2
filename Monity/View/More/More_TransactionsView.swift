1ba1cfef381d40a17d18f08ab93b5d09f4b72a70
Niklas Kuder <nkqdr@users.noreply.github.com>
2023-10-18 11:23:22 +0200

    Added budget levels and slightly adjusted monthly limit setting process

--- Monity/View/More/More_TransactionsView.swift
+++ Monity/View/More/More_TransactionsView.swift
@@ -7,15 +7,167 @@
 
 import SwiftUI
 
-struct More_TransactionsView: View {
+fileprivate struct BudgetLevel {
+    var title: LocalizedStringKey
+    var emoji: String
+    var description: LocalizedStringKey
+    var rangeMin: Int
+    var rangeMax: Int
+    
+    static func getDetails(for amount: Double) -> BudgetLevel? {
+        let lowerAmount = Int(floor(amount))
+        return budgetLevels.first(where: { $0.rangeMin <= lowerAmount && lowerAmount < $0.rangeMax})
+    }
+}
+
+fileprivate let budgetLevels: [BudgetLevel] = [
+    BudgetLevel(
+        title: "Smart Saver",
+        emoji: "💸",
+        description: "Mastering the art of saving and shining bright! Low expenses, high savings!",
+        rangeMin: 0, rangeMax: 500),
+    BudgetLevel(
+        title: "Thrifty Achiever",
+        emoji: "🎩",
+        description: "Making every penny count and managing expenses wisely. Spending smartly, saving cleverly!",
+        rangeMin: 500, rangeMax: 1000),
+    BudgetLevel(
+        title: "Wealth Builder",
+        emoji: "💵",
+        description:  "Turning financial dreams into reality without breaking the bank. Moderate expenses, maximum aspirations!",
+        rangeMin: 1000, rangeMax: 2000),
+    BudgetLevel(
+        title: "Prosperity Pioneer",
+        emoji: "🌟",
+        description: "Navigating wealth and expenses with grace. Living comfortably, dreaming extravagantly!",
+        rangeMin: 2000, rangeMax: 3000),
+    BudgetLevel(
+        title: "Cash Champion",
+        emoji: "👑",
+        description: "Swimming in money and still knowing the value! Living a life of luxury, with expenses as big as diamonds and a bank balance as deep as the ocean!",
+        rangeMin: 3000, rangeMax: 5000),
+    BudgetLevel(
+        title: "Platinum Prestige",
+        emoji: "🏰",
+        description: "Living in opulence without financial strain. Opulent expenses, grandeur on a budget!",
+        rangeMin: 5000, rangeMax: 10000),
+    BudgetLevel(
+        title: "Billionaire Luminary",
+        emoji: "💎",
+        description: "Building an empire of wealth and dreams. Sky's the limit for expenses, bank balance reaching for the stars!",
+        rangeMin: 10000, rangeMax: Int.max),
+]
+
+fileprivate struct SetLimitSheet: View {
+    @Binding var isPresented: Bool
+    @FocusState private var limitInputIsFocussed: Bool
+    @State private var tmpMonthlyLimit: Double = UserDefaults.standard.double(forKey: AppStorageKeys.monthlyLimit)
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Limit", value: $tmpMonthlyLimit, format: .customCurrency())
+                        .keyboardType(.decimalPad)
+                        .focused($limitInputIsFocussed)
+                } header: {
+                    Text("Monthly limit")
+                }
+            }
+            .onAppear {
+                limitInputIsFocussed = true
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save") {
+                        withAnimation {
+                            UserDefaults.standard.set(tmpMonthlyLimit, forKey: AppStorageKeys.monthlyLimit)
+                        }
+                        isPresented.toggle()
+                    }
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel", role: .cancel, action: {
+                        isPresented.toggle()
+                    })
+                }
+            }
+        }
+    }
+}
+
+fileprivate struct MonthlyLimitSection: View {
     @AppStorage(AppStorageKeys.monthlyLimit) private var monthlyLimit: Double?
-    @StateObject private var content = SettingsTransactionsViewModel()
     @State private var showingEditAlert: Bool = false
     @State private var showingDeleteConfirmation: Bool = false
     
+    private var budgetLevel: BudgetLevel? {
+        BudgetLevel.getDetails(for: monthlyLimit ?? -1)
+    }
+    
+    private var monthlyLimitFooter: some View {
+        Text("Set yourself a monthly limit and try to stay in your own budget")
+    }
+    
+    var body: some View {
+        Section(footer: monthlyLimitFooter) {
+            if let budgetLevel {
+                HStack(alignment: .top) {
+                    Text(budgetLevel.emoji)
+                        .font(.largeTitle)
+                    VStack(alignment: .leading) {
+                        Text(budgetLevel.title).font(.headline.bold())
+                        Text(budgetLevel.description).font(.footnote).foregroundStyle(.secondary)
+                    }
+                }
+            }
+            HStack {
+                Text("Your monthly limit:")
+                Spacer()
+                if let limit = monthlyLimit {
+                    Text(limit, format: .customCurrency())
+                        .foregroundColor(.green)
+                        .fontWeight(.bold)
+                } else {
+                    Text("-")
+                        .foregroundColor(.gray)
+                }
+            }
+            HStack {
+                Button("Change limit") {
+                    showingEditAlert.toggle()
+                }
+                .buttonStyle(.borderless)
+                Spacer()
+                Button("Delete limit", role: .destructive) {
+                    showingDeleteConfirmation.toggle()
+                }
+                .buttonStyle(.borderless)
+            }
+        }
+        .sheet(isPresented: $showingEditAlert) {
+            SetLimitSheet(isPresented: $showingEditAlert)
+                .presentationDetents([.height(200)])
+        }
+        .confirmationDialog("Delete monthly limit", isPresented: $showingDeleteConfirmation, titleVisibility: .visible) {
+            Button("Delete", role: .destructive) {
+                withAnimation(.easeInOut) {
+                    UserDefaults.standard.removeObject(forKey: AppStorageKeys.monthlyLimit)
+                }
+            }
+        } message: {
+            Text("This has no effect on your stored transactions.")
+        }
+        .textCase(nil) // To avoid dialogs appearing in all-uppercase
+    }
+}
+
+struct More_TransactionsView: View {
+    @StateObject private var content = SettingsTransactionsViewModel()
+    
     var body: some View {
         EditableDeletableItemList(viewModel: content) { create, edit, delete in
-            monthlyLimitSection
+            MonthlyLimitSection()
             Section(header: categorySectionHeader(create), footer: categorySectionFooter) {
                 ForEach(content.items) { category in
                     EditableDeletableItem(
@@ -41,58 +193,6 @@ struct More_TransactionsView: View {
         }
         .navigationTitle("Transactions")
         .navigationBarTitleDisplayMode(.inline)
-        .alert("Set monthly limit", isPresented: $showingEditAlert, actions: {
-            TextField("Limit", value: $content.monthlyLimit, format: .customCurrency())
-            Button("Save") {
-                UserDefaults.standard.set(content.monthlyLimit, forKey: AppStorageKeys.monthlyLimit)
-            }
-            Button("Cancel", role: .cancel, action: {})
-        }, message: {
-            Text("Please enter your desired limit.")
-        })
-    }
-    
-    private var monthlyLimitHeader: some View {
-        Text("Monthly limit")
-    }
-    
-    private var monthlyLimitFooter: some View {
-        Text("Set yourself a monthly limit and try to stay in your own budget")
-    }
-    
-    private var monthlyLimitSection: some View {
-        Section(header: monthlyLimitHeader, footer: monthlyLimitFooter) {
-            HStack {
-                Text("Your monthly limit:")
-                Spacer()
-                if let limit = monthlyLimit {
-                    Text(limit, format: .customCurrency())
-                        .foregroundColor(.green)
-                        .fontWeight(.bold)
-                } else {
-                    Text("-")
-                        .foregroundColor(.gray)
-                }
-            }
-            HStack {
-                Button("Change limit") {
-                    showingEditAlert.toggle()
-                }
-                .buttonStyle(.borderless)
-                Spacer()
-                Button("Delete limit", role: .destructive) {
-                    showingDeleteConfirmation.toggle()
-                }
-                .buttonStyle(.borderless)
-            }
-        }
-        .confirmationDialog("Delete monthly limit", isPresented: $showingDeleteConfirmation) {
-            Button("Delete", role: .destructive) {
-                withAnimation(.easeInOut) {
-                    UserDefaults.standard.removeObject(forKey: AppStorageKeys.monthlyLimit)
-                }
-            }
-        }
     }
     
     func categorySectionHeader(_ createFunc: @escaping () -> Void) -> some View {
