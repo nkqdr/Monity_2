d52672d1833ded45ad7687333af4472eee0a28f8
Niklas Kuder <nkqdr@users.noreply.github.com>
2023-11-01 21:00:22 +0100

    Grouped savings entry lists into yearly sections

--- Monity/View/SavingsView/SavingsCategoryListView.swift
+++ Monity/View/SavingsView/SavingsCategoryListView.swift
@@ -7,36 +7,56 @@
 
 import SwiftUI
 
+fileprivate struct EntryGroup: Identifiable {
+    var id = UUID()
+    var date: Date
+    var entries: [SavingsEntry]
+}
+
 fileprivate struct SavingsEntryList: View {
     var category: SavingsCategory
     var content: SavingsViewModel
+    
+    var groupedEntries: [EntryGroup] {
+        let groupingDict = Dictionary(grouping: content.items) { entry in
+            return Calendar.current.component(.year, from: entry.wrappedDate)
+        }
+        return groupingDict.map { (key, val) in
+            EntryGroup(date: val.first!.wrappedDate, entries: val)
+        }.sorted {
+            $0.date > $1.date
+        }
+    }
     
     var body: some View {
         EditableDeletableItemList(
             viewModel: content
         ) { create, edit, delete in
-            
-            Section(header: Text("Entries")) {
-                ForEach(content.items) { entry in
-                    EditableDeletableItem(
-                        item: entry,
-                        confirmationTitle: "Are you sure you want to delete this entry?",
-                        confirmationMessage: "This cannot be undone!",
-                        onEdit: edit,
-                        onDelete: delete) { item in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(item.wrappedDate, format: .dateTime.year().month().day())
-                                Text(item.wrappedDate, format: .dateTime.hour().minute())
-                                    .foregroundColor(.secondary)
-                                    .font(.footnote)
+            ForEach(groupedEntries) { groupedEntry in
+                Section {
+                    ForEach(groupedEntry.entries) { entry in
+                        EditableDeletableItem(
+                            item: entry,
+                            confirmationTitle: "Are you sure you want to delete this entry?",
+                            confirmationMessage: "This cannot be undone!",
+                            onEdit: edit,
+                            onDelete: delete) { item in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.wrappedDate, format: .dateTime.year().month().day())
+                                    Text(item.wrappedDate, format: .dateTime.hour().minute())
+                                        .foregroundColor(.secondary)
+                                        .font(.footnote)
+                                }
+                                Spacer()
+                                Text(item.amount, format: .customCurrency())
+                                    .foregroundColor(item.amount >= 0 ? .green : .red)
                             }
-                            Spacer()
-                            Text(item.amount, format: .customCurrency())
-                                .foregroundColor(item.amount >= 0 ? .green : .red)
+                            .padding(.vertical, 2)
                         }
-                        .padding(.vertical, 2)
                     }
+                } header: {
+                    Text(groupedEntry.date, format: .dateTime.year())
                 }
             }
         } sheetContent: { showSheet, currentItem in
