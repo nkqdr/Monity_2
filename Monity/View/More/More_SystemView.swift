d7d36737c7272204fceb7bac001a099a3fc012e8
Niklas Kuder <nkqdr@users.noreply.github.com>
2023-11-07 22:45:03 +0100

    Improved UX of the csv import process

--- Monity/View/More/More_SystemView.swift
+++ Monity/View/More/More_SystemView.swift
@@ -41,11 +41,19 @@ struct StoredItemsTile: View {
 }
 
 struct ImportSummaryView: View {
-    var summary: ImportCSVSummary
-    var onImport: () -> Void
-    var onCancel: () -> Void
+    @Binding var summary: ImportCSVSummary?
     
-    var body: some View {
+    @ViewBuilder
+    var loadingScreen: some View {
+        VStack {
+            Spacer()
+            ProgressView()
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    func SummaryView(summary: ImportCSVSummary) -> some View {
         VStack {
             HStack {
                 VStack(alignment: .leading) {
@@ -59,7 +67,7 @@ struct ImportSummaryView: View {
                 }
                 Spacer()
                 VStack(alignment: .trailing) {
-                    Text("Rows:")
+                    Text("Entries:")
                         .foregroundColor(.secondary)
                         .font(.subheadline)
                         .fontWeight(.semibold)
@@ -69,24 +77,25 @@ struct ImportSummaryView: View {
                 }
             }
             .padding()
-            ScrollView {
-                ForEach(summary.rows, id: \.self) { row in
-                    ImportSummaryRow(resource: summary.resource, row: row)
+            GeometryReader { proxy in
+                ScrollView {
+                    LazyVStack {
+                        ForEach(summary.rows, id: \.self) { row in
+                            ImportSummaryRow(resource: summary.resource, row: row)
+                        }
+                    }
+                    .padding()
                 }
-                .padding()
+                .frame(maxHeight: proxy.size.height - 80)
             }
-            HStack {
-                Button("Cancel", role: .destructive) {
-                    onCancel()
-                }
-                .buttonStyle(.borderless)
-                Spacer()
-                Button("Import") {
-                    onImport()
-                }
-                .buttonStyle(.borderedProminent)
-            }
-            .padding()
+        }
+    }
+    
+    var body: some View {
+        if let summary {
+            SummaryView(summary: summary)
+        } else {
+            loadingScreen
         }
     }
 }
@@ -173,11 +182,147 @@ fileprivate struct ExportSection: View {
     }
 }
 
+fileprivate struct ImportCSVWizard: View {
+    @Environment(\.dismiss) var dismiss
+    @StateObject private var csvImporter = CSVImporter()
+    @State private var selectedStructurePreview: CSVValidHeaders = CSVValidHeaders.transactionCSV
+    @State private var currentlySelectedPageIndex: Int = 0
+    
+    private var tableColumns: [Substring] {
+        selectedStructurePreview.rawValue.split(separator: ",")
+    }
+    
+    @ViewBuilder
+    var firstPage: some View {
+        ScrollView {
+            VStack {
+                Text("⤵️").font(.largeTitle).padding(.bottom, 10).padding(.top, 120)
+                Text("Import CSV data")
+                    .font(.title.bold())
+                    .padding(.bottom, 30)
+                Text("Import data seamlessly from previous versions of Monity or any compatible app. As long as the file meets our import standards, your financial data integration is hassle-free.")
+                    .foregroundStyle(.secondary)
+                    .padding(.horizontal)
+                HStack {
+                    if csvImporter.isReading {
+                        ProgressView()
+                    } else if let summary = csvImporter.importSummary, !csvImporter.importHasError {
+                        Image(systemName: "checkmark.circle.fill")
+                            .font(.system(size: 40))
+                            .foregroundStyle(.green)
+                        VStack(alignment: .leading) {
+                            Text("File successfully read")
+                            Group {
+                                Text("Type of data: ") + Text(summary.resourceName)
+                            }
+                            .font(.footnote).foregroundStyle(.secondary)
+                        }
+                        Spacer()
+                    } else if csvImporter.importHasError {
+                        Image(systemName: "xmark.circle.fill")
+                            .font(.system(size: 40))
+                            .foregroundStyle(.red)
+                        VStack(alignment: .leading) {
+                            Text("File could not be read")
+                            Text("Please ensure that the selected file has the correct format.").font(.footnote).foregroundStyle(.secondary)
+                        }
+                        .multilineTextAlignment(.leading)
+                        Spacer()
+                    } else {
+                        Text("Please select a .csv file").font(.subheadline).foregroundStyle(.secondary)
+                    }
+                }
+                .padding()
+                .frame(maxWidth: .infinity, minHeight: 80)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 10))
+                .padding()
+                Button {
+                    csvImporter.showDocumentPicker.toggle()
+                } label: {
+                    Label("Select CSV", systemImage: "doc")
+                }
+                .buttonStyle(.bordered)
+                .disabled(csvImporter.isReading)
+            }
+        }
+        .scrollIndicators(.hidden)
+        .multilineTextAlignment(.center)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            HStack(spacing: 0) {
+                firstPage
+                    .frame(width: UIScreen.main.bounds.width)
+                ImportSummaryView(summary: $csvImporter.importSummary)
+                    .frame(width: UIScreen.main.bounds.width)
+            }
+            .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+            .offset(x: UIScreen.main.bounds.width * -CGFloat(currentlySelectedPageIndex), y: 0)
+            .animation(.easeInOut, value: currentlySelectedPageIndex)
+            
+            // MARK: Buttons
+            HStack {
+                Button {
+                    Haptics.shared.play(.soft)
+                    if currentlySelectedPageIndex == 0 {
+                        dismiss()
+                    } else {
+                        withAnimation {
+                            currentlySelectedPageIndex -= 1
+                        }
+                    }
+                } label: {
+                    Image(systemName: currentlySelectedPageIndex == 0 ? "xmark" : "chevron.backward")
+                        .padding(5)
+                }
+                .buttonStyle(.bordered)
+                .tint(currentlySelectedPageIndex == 0 ? .red : nil)
+                .disabled(csvImporter.isReading)
+                Spacer()
+                Button {
+                    Haptics.shared.play(.soft)
+                    if currentlySelectedPageIndex == 0 {
+                        withAnimation {
+                            currentlySelectedPageIndex += 1
+                        }
+                    } else {
+                        csvImporter.importCSV()
+                        dismiss()
+                    }
+                } label: {
+                    Group {
+                        if currentlySelectedPageIndex == 0 {
+                            Image(systemName: "chevron.forward")
+                        } else {
+                            Label("Import", systemImage: "chevron.forward")
+                        }
+                    }
+                    .padding(5)
+                }
+                .buttonStyle(.bordered)
+                .disabled(csvImporter.importSummary == nil)
+                .tint(currentlySelectedPageIndex == 1 ? .green : nil)
+            }
+            .padding()
+        }
+        .alert("Could not read this file", isPresented: $csvImporter.showInvalidFileAlert) {
+            Button("Try again") {
+                csvImporter.showInvalidFileAlert.toggle()
+            }
+        } message: {
+            Text("Please make sure to use a csv file with the correct format.")
+        }
+        .sheet(isPresented: $csvImporter.showDocumentPicker) {
+            DocumentPicker(fileContent: $csvImporter.csvFileContent)
+                .ignoresSafeArea()
+        }
+    }
+}
+
 struct More_SystemView: View {
     @StateObject private var content = SettingsSystemViewModel()
     @State private var showDeleteAllConfirmation: Bool = false
-    // These are needed because an error occurs when directly using the value in the ViewModel
-    @State private var importSummary: ImportCSVSummary?
     
     var body: some View {
         List {
@@ -185,24 +330,10 @@ struct More_SystemView: View {
             importSection
             ExportSection()
         }
-        .sync($content.importSummary, with: $importSummary)
+        
         .sheet(isPresented: $content.showFilePicker) {
-            DocumentPicker(fileContent: $content.csvFileContent)
-                .ignoresSafeArea()
-        }
-        .sheet(item: $importSummary) { summary in
-            ImportSummaryView(summary: summary, onImport: {
-                content.importCSV()
-            }, onCancel: {
-                content.importSummary = nil
-            })
-        }
-        .alert("Could not read this file", isPresented: $content.showInvalidFileAlert) {
-            Button("Try again") {
-                content.showInvalidFileAlert.toggle()
-            }
-        } message: {
-            Text("Please make sure to use a csv file with the correct format.")
+            ImportCSVWizard()
+                .interactiveDismissDisabled()
         }
         .confirmationDialog("Are you sure you want to delete all App-Data?", isPresented: $showDeleteAllConfirmation, titleVisibility: .visible) {
             Button("Delete", role: .destructive) {
@@ -258,7 +389,7 @@ struct More_SystemView: View {
             Button {
                 content.showFilePicker.toggle()
             } label: {
-                Label("Select CSV", systemImage: "doc")
+                Label("Select CSV", systemImage: "square.and.arrow.down")
             }
         }
     }
@@ -266,6 +397,6 @@ struct More_SystemView: View {
 
 struct Settings_SystemView_Previews: PreviewProvider {
     static var previews: some View {
-        More_SystemView()
+        ImportCSVWizard()
     }
 }
--- Monity/ViewModel/SettingsSystemViewModel.swift
+++ Monity/ViewModel/SettingsSystemViewModel.swift
@@ -8,48 +8,20 @@
 import Foundation
 import Combine
 
-class SettingsSystemViewModel: ObservableObject {
-    @Published var isWorking: Bool = false
-    @Published var storageUsedString: String = PersistenceController.shared.getSqliteStoreSize()
-    @Published var showFilePicker: Bool = false
-    @Published var showInvalidFileAlert: Bool = false
+class CSVImporter: ObservableObject {
     @Published var importSummary: ImportCSVSummary?
-    @Published var totalTransactionCount: Int = 0
-    @Published var totalSavingsCount: Int = 0
-    @Published var totalRecurringTransactionCount: Int = 0
+    @Published var importHasError: Bool = false
+    @Published var isReading: Bool = false
+    @Published var showDocumentPicker: Bool = false
     @Published var csvFileContent: String = "" {
         didSet {
-            let rows = csvFileContent.split(whereSeparator: \.isNewline)
-            let header: CSVValidHeaders? = CSVValidHeaders.fromValue(String(rows.first ?? ""))
-            guard let header else {
-                importSummary = nil
-                showInvalidFileAlert.toggle()
-                return
+            if !csvFileContent.isEmpty {
+                handleContentChange()
             }
-            importSummary = ImportCSVSummary(resource: header, rowsAmount: rows.count-1, rows: rows[1...].map { String($0) })
         }
     }
+    @Published var showInvalidFileAlert: Bool = false
     
-    private var transactionCancellable: AnyCancellable?
-    private var recurringTransactionCancellable: AnyCancellable?
-    private var savingsCancellable: AnyCancellable?
-    
-    init() {
-        let transactionPublisher = TransactionFetchController.all.items.eraseToAnyPublisher()
-        self.transactionCancellable = transactionPublisher.sink { values in
-            self.totalTransactionCount = values.count
-        }
-        
-        let recurringTransactionPublisher = RecurringTransactionFetchController.all.items.eraseToAnyPublisher()
-        self.recurringTransactionCancellable = recurringTransactionPublisher.sink { values in
-            self.totalRecurringTransactionCount = values.count
-        }
-        
-        let savingsPublisher = SavingsFetchController.all.items.eraseToAnyPublisher()
-        self.savingsCancellable = savingsPublisher.sink { values in
-            self.totalSavingsCount = values.count
-        }
-    }
     
     // MARK: - Helper functions
     func importTransactionsCSV(_ rows: [String]) {
@@ -73,6 +45,29 @@ class SettingsSystemViewModel: ObservableObject {
         }
     }
     
+    func handleContentChange() {
+        isReading = true
+        importSummary = nil
+        DispatchQueue.global(qos: .userInteractive).async {
+            let rows = self.csvFileContent.split(whereSeparator: \.isNewline)
+            let header: CSVValidHeaders? = CSVValidHeaders.fromValue(String(rows.first ?? ""))
+            guard let header else {
+                DispatchQueue.main.async {
+                    self.importSummary = nil
+                    self.importHasError = true
+                    self.showInvalidFileAlert.toggle()
+                    self.isReading = false
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.importSummary = ImportCSVSummary(resource: header, rowsAmount: rows.count-1, rows: rows[1...].map { String($0) })
+                self.isReading = false
+                self.importHasError = false
+            }
+        }
+    }
+    
     // MARK: - Intents
     
     func importCSV() {
@@ -88,9 +83,40 @@ class SettingsSystemViewModel: ObservableObject {
                 self.importRecurringTransactionsCSV(summary.rows)
             }
         }
-        // End with this to close the sheet
-        importSummary = nil
     }
+    
+}
+
+class SettingsSystemViewModel: ObservableObject {
+    @Published var isWorking: Bool = false
+    @Published var storageUsedString: String = PersistenceController.shared.getSqliteStoreSize()
+    @Published var showFilePicker: Bool = false
+    @Published var totalTransactionCount: Int = 0
+    @Published var totalSavingsCount: Int = 0
+    @Published var totalRecurringTransactionCount: Int = 0
+    
+    private var transactionCancellable: AnyCancellable?
+    private var recurringTransactionCancellable: AnyCancellable?
+    private var savingsCancellable: AnyCancellable?
+    
+    init() {
+        let transactionPublisher = TransactionFetchController.all.items.eraseToAnyPublisher()
+        self.transactionCancellable = transactionPublisher.sink { values in
+            self.totalTransactionCount = values.count
+        }
+        
+        let recurringTransactionPublisher = RecurringTransactionFetchController.all.items.eraseToAnyPublisher()
+        self.recurringTransactionCancellable = recurringTransactionPublisher.sink { values in
+            self.totalRecurringTransactionCount = values.count
+        }
+        
+        let savingsPublisher = SavingsFetchController.all.items.eraseToAnyPublisher()
+        self.savingsCancellable = savingsPublisher.sink { values in
+            self.totalSavingsCount = values.count
+        }
+    }
+    
+    // MARK: - Intents
     
     func deleteTransactionData() {
         TransactionStorage.main.deleteAll()
