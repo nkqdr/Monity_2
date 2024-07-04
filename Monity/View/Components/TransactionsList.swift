8c334028b71f37c34d732f0636e41ddb4226ba2a
Niklas Kuder <nkqdr@users.noreply.github.com>
2024-07-04 07:55:10 +0200

    Refactored TransactionsList in order to be able to add a new item to the context menu

--- Monity/View/Components/TransactionsList.swift
+++ Monity/View/Components/TransactionsList.swift
@@ -7,20 +7,76 @@
 
 import SwiftUI
 
+fileprivate struct TransactionListTile: View {
+    @ObservedObject var transaction: Transaction
+    @State var showEditView: Bool = false
+    @State private var showConfirmationDialog: Bool = false
+    
+    var body: some View {
+        HStack {
+            if let icon = transaction.category?.iconName {
+                Image(systemName: icon)
+                    .padding(.trailing, 10)
+                    .font(.title3)
+                    .foregroundStyle(.secondary)
+            }
+            VStack(alignment: .leading) {
+                Text(LocalizedStringKey(transaction.category?.wrappedName ?? "No category"))
+                    .font(.headline)
+                Text(transaction.wrappedText)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+            Text(transaction.amount, format: .customCurrency())
+                .foregroundColor(transaction.isExpense ? .red : .green)
+        }
+        .deleteSwipeAction {
+            showConfirmationDialog.toggle()
+        }
+        .editSwipeAction {
+            showEditView.toggle()
+        }
+        .contextMenu {
+            Button {
+                showEditView.toggle()
+            } label: {
+                Label("Edit", systemImage: "pencil")
+            }
+            Button(role: .destructive) {
+                showConfirmationDialog.toggle()
+            } label: {
+                Label("Delete", systemImage: "trash")
+            }
+        }
+        .sheet(isPresented: $showEditView) {
+            AddTransactionView(
+                isPresented: $showEditView,
+                editor: TransactionEditor(transaction: transaction)
+            )
+            .presentationDetents([.medium, .large])
+            .presentationDragIndicator(.hidden)
+        }
+        .confirmationDialog(
+            "Delete transaction",
+            isPresented: $showConfirmationDialog,
+            titleVisibility: .visible
+        ) {
+            Button("Delete", role: .destructive) {
+                withAnimation(.easeInOut) {
+                    TransactionStorage.main.delete(transaction)
+                }
+            }
+        } message: {
+            Text("Are you sure you want to delete this transaction?")
+        }
+    }
+}
+
 struct TransactionsList: View {
-    @State private var currentTransaction: Transaction?
     @Binding var showAddTransactionView: Bool
     var transactionsByDate: [TransactionsByDate]
     var dateFormat: Date.FormatStyle = .dateTime.year().month().day()
-    
-    func deleteTransaction(_ transaction: Transaction) {
-        TransactionStorage.main.delete(transaction)
-    }
-    
-    func showEditSheetForTransaction(_ transaction: Transaction) {
-        currentTransaction = transaction
-        showAddTransactionView.toggle()
-    }
     
     func getTransactionSumFor(_ list: [Transaction], isExpense: Bool) -> Double {
         return list.filter({ $0.isExpense == isExpense }).map({ $0.amount }).reduce(0, +)
@@ -45,43 +101,14 @@ struct TransactionsList: View {
         List(transactionsByDate) { date in
             Section(header: sectionHeaderFor(date)) {
                 ForEach(date.transactions) { transaction in
-                    EditableDeletableItem(
-                        item: transaction,
-                        confirmationTitle: "Delete transaction",
-                        confirmationMessage: "Are you sure you want to delete this transaction?",
-                        onEdit: showEditSheetForTransaction,
-                        onDelete: deleteTransaction) { item in
-                            HStack {
-                                if let icon = transaction.category?.iconName {
-                                    Image(systemName: icon)
-                                        .padding(.trailing, 10)
-                                        .font(.title3)
-                                        .foregroundStyle(.secondary)
-                                }
-                                VStack(alignment: .leading) {
-                                    Text(LocalizedStringKey(item.category?.wrappedName ?? "No category"))
-                                        .font(.headline)
-                                    Text(item.wrappedText)
-                                        .font(.subheadline)
-                                        .foregroundColor(.secondary)
-                                }
-                                Spacer()
-                                Text(item.amount, format: .customCurrency())
-                                    .foregroundColor(item.isExpense ? .red : .green)
-                            }
-                    }
+                    TransactionListTile(transaction: transaction)
                 }
             }
         }
         .sheet(isPresented: $showAddTransactionView) {
-            AddTransactionView(isPresented: $showAddTransactionView, editor: TransactionEditor(transaction: currentTransaction))
+            AddTransactionView(isPresented: $showAddTransactionView, editor: TransactionEditor(transaction: nil))
                 .presentationDetents([.medium, .large])
                 .presentationDragIndicator(.hidden)
-        }
-        .onChange(of: showAddTransactionView) { newValue in
-            if !newValue {
-                currentTransaction = nil
-            }
         }
     }
 }
