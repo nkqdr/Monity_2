ab2fbb9a1defb16d84d0b6a3d59a45a4aab90121
Niklas Kuder <nkqdr@users.noreply.github.com>
2024-07-10 19:28:28 +0200

    Added basic model for category budgets

---
+++ Monity/Model/BudgetStorage.swift
@@ -0,0 +1,50 @@
+//
+//  BudgetStorage.swift
+//  Monity
+//
+//  Created by Niklas Kuder on 10.07.24.
+//
+
+import Foundation
+import CoreData
+
+class BudgetFetchController: BaseFetchController<Budget> {
+    init(
+        predicate: NSPredicate? = nil,
+        managedObjectContext: NSManagedObjectContext = PersistenceController.shared.managedObjectContext
+    ) {
+        super.init(
+            sortDescriptors: [NSSortDescriptor(keyPath: \Budget.validFrom, ascending: true)],
+            predicate: predicate,
+            managedObjectContext: managedObjectContext
+        )
+    }
+    
+    convenience init(
+        for category: TransactionCategory,
+        managedObjectContext: NSManagedObjectContext = PersistenceController.shared.managedObjectContext
+    ) {
+        self.init(
+            predicate: NSPredicate(format: "category == %@", category),
+            managedObjectContext: managedObjectContext
+        )
+    }
+}
+
+class BudgetStorage: ResettableStorage<Budget> {
+    static let main: BudgetStorage = BudgetStorage(
+        managedObjectContext: PersistenceController.shared.managedObjectContext
+        )
+    
+    func add(amount: Double, category: TransactionCategory?, validFrom: Date = Date()) -> Budget {
+        self.context.performAndWait {
+            let budget = Budget(context: self.context)
+            budget.id = UUID()
+            budget.validFrom = validFrom
+            budget.amount = amount
+            budget.category = category
+            try? self.context.save()
+            return budget
+        }
+    }
+}
